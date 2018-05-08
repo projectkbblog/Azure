a1e41aab35adf58318af8c07c8dd976c0a8fe4a1
Project KB Blog <projectkbblog@outlook.com>
2018-05-08 15:40:33 +1000

    Initial draft of Kerberos key rollover
    Inital draft version of Kerberos Key Rollover script for Azure Seamless SSO

---
+++ Scripts/Seamless SSO/KerberosKeyRollover.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [Parameter(mandatory=$false)]
+    [string] $OnPremAdmin,
+    [Parameter(mandatory=$false)]
+    [string] $AzureAdmin
+)
+
+$OnPremAdministrator = Get-Credential -Message "Enter on-premises domain administrator credentials" -UserName $OnPremAdmin
+$CloudAdmin = Get-Credential -Message "Enter Azure AD administrator credentials" -UserName $AzureAdmin
+
+Import-Module 'C:\Program Files\Microsoft Azure Active Directory Connect\AzureADSSO.psd1'
+New-AzureADSSOAuthenticationContext -CloudCredentials $CloudCred
+Update-AzureADSSOForest -OnPremCredentials $OnpremCred
